bb07e70b2c0a9499c08ad19378eeb277f381b26b
Konstantin <reverant_01@mail.ru>
2024-04-27 22:35:30 +0300

    На проверку №1

---
+++ src/main/java/com/collections/stream/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.collections.stream;
+
+import java.util.List;
+import java.util.Map;
+
+public interface EmployeeService {
+    String addEmployee(String fio, int dep, int salary);
+    String findEmployee(String fio);
+    String deleteEmployee(String fio);
+    Employee maxSalary(int departmentId);
+    Employee minSalary(Integer departmentId);
+    List<Employee> getAllEmployeesOfDepartment(int departmentId);
+    Map<Integer, List<Employee>> allEmployees();
+
+}
